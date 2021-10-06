884c27a08e7919e877f28f8eaa68afba79919335
In Niko <inniko9111@gmail.com>
2021-10-06 12:29:40 +0300

    подключает файл modal.js на страницы

---
+++ js/modal.js
@@ -0,0 +1,13 @@
+(() => {
+  const refs = {
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
